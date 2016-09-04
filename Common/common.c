21f399a88486c7dc39d9522bc1aec5bd3ef6e06c
DMavrotas <dmavrotas@hotmail.com>
2016-09-04 20:08:26 +0200

    Common functions

---
+++ Common/common.c
@@ -0,0 +1,59 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/* This function creates a matrix and returns it */
+Matrix* CreateMatrix(int sizex, int sizey, double* dataz) {
+    int i = 0;
+    int j = 0;
+
+    if(sizex == 0 || sizey == 0) return NULL;
+
+    if(dataz == NULL) return NULL;
+
+    Matrix* matrix = NULL;
+
+    matrix = malloc(sizeof(Matrix));
+    if(matrix == NULL) return NULL;
+
+    matrix->sizex = sizex;
+    matrix->sizey = sizey;
+    matrix->items = malloc(sizex * sizeof(MatrixItem*));
+    if(matrix->items == NULL) return NULL;
+
+    for(i = 0; i < sizex; i++) {
+        matrix->items[i] = malloc(sizey * sizeof(MatrixItem));
+        if(matrix->items[i] == NULL) return NULL;
+        for(j = 0; j < sizey; j++) {
+            if(matrix->items[i][j] != NULL) {
+                matrix->items[i][j]->row = i;
+                matrix->items[i][j]->column = j;
+                matrix->items[i][j]->data = dataz;
+            } 
+        }
+    }
+}
+
+/* This function inserts a MatrixItem inside a Matrix and returns the matrix */
+Matrix** InsertMatrixItem(Matrix** matrix, MatrixItem* item) {
+
+}
+
+/* This function just prints a matrix */
+void PrintMatrix(Matrix** matrix) {
+    int i = 0;
+    int j = 0;
+
+    if(matrix == NULL) return;
+
+    if(matrix->sizex == 0 || matrix->sizey == 0) return;
+
+    for(i = 0; i < matrix->sizex; i++) {
+        for(j = 0; j < matrix->sizey; j++) {
+            if((matrix->items[i][j])->data[0] != NULL) {
+                printf(" %2f ", (matrix->items[i][j])->data[0]);
+            }
+        }
+        printf("\n");
+    }
+}
+
---
+++ Common/common.h
@@ -0,0 +1,20 @@
+/* Represantation of the Matrix inside the program, along with its functions */
+
+struct MatrixItem {
+    int row;
+    int column;
+    double* data;
+};
+
+struct Matrix {
+    int sizex;
+    int sizey;
+    MatrixItem** items;
+};
+
+/* Matrix functions */
+
+Matrix** CreateMatrix(int sizex, int sizey, double* dataz);
+Matrix** InsertMatrixItem(Matrix** matrix, MatrixItem* item)
+void PrintMatrix(Matrix** matrix);
+
